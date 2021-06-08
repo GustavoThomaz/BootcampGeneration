6097254e18af1d93223e1c2f0213e163d8a8e566
Gustavo <gustavo.thomaz.ismart@gmail.com>
2021-06-08 13:07:21 -0300

    Feat. Adicionando segurança em modelo basic

---
+++ Spring Boot/blogPessoal/src/main/java/org/generation/blogPessoal/controller/UsuarioController.java
@@ -0,0 +1,37 @@
+package org.generation.blogPessoal.controller;
+
+import java.util.Optional;
+
+import org.generation.blogPessoal.model.UserLogin;
+import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+@CrossOrigin (origins = "*", allowedHeaders = "*")
+public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@PostMapping("/logar")
+	public ResponseEntity<UserLogin> Autentication (@RequestBody Optional<UserLogin> user){
+		return usuarioService.Logar(user).map(ret -> ResponseEntity.status(200).body(ret))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+	
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> Post (@RequestBody Usuario usuario){
+		return ResponseEntity.status(201).body(usuarioService.CadastrarUsuario(usuario));
+	}
+	
+	
+}
